938db810ca1eed418b063fc2999bc2a2a8cdb23c
obibaadoma <akrasikonadu@yahoo.com>
2023-07-14 12:10:48 +0000

    Add Remove to-do-list

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import _ from 'lodash';
 /* eslint-enable */
 import './style.css';
 import { addList, displayList } from './modules/user.js';
-import clearCompleted from './modules/clearAll.js';
 import localGet from './modules/localStorage.js';
 
 const form = document.querySelector('.list-con');
 const todoTask = document.getElementById('input');
-const clearAll = document.getElementById('Completedbtn');
 
 window.addEventListener('load', () => {
   displayList();
@@ -17,11 +15,5 @@ window.addEventListener('load', () => {
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   addList(todoTask.value, false, localGet().length + 1);
-  displayList();
-});
-
-clearAll.addEventListener('click', (e) => {
-  e.preventDefault();
-  clearCompleted();
   displayList();
 });
